25e36a26ba5bc2d7a9a352ec5e30bf7b666c2d12
PLippmann <p.lippmann@tudelft.nl>
2024-11-13 15:07:31 +0100

    Changed prepare script to create .bin files and chunk data to fit in RAM

--- data/openwebtext/prepare.py
+++ data/openwebtext/prepare.py
@@ -1,21 +1,13 @@
-# Copied from https://github.com/karpathy/nanoGPT/blob/177d5f7dc5f44d6f373cd7767c2a9259d740436e/data/openwebtext/prepare.py
-# saves the openwebtext dataset to a binary file for training. following was helpful:
-# https://github.com/HazyResearch/flash-attention/blob/main/training/src/datamodules/language_modeling_hf.py
-
-# to read the bin files later, e.g. with numpy:
-# m = np.memmap('train.bin', dtype=np.uint16, mode='r')
-
 from multiprocessing import cpu_count
 from tqdm import tqdm
 import numpy as np
 import tiktoken
-from datasets import load_dataset, DatasetDict
-import tensorflow as tf
+from datasets import load_dataset
 
 # number of workers in .map() call
 # good number to use is ~order number of cpu cores // 2
 num_proc = cpu_count() // 2
-num_shards = {'train': 32, 'val': 8}
+print(f"Using {num_proc} processes for {cpu_count()} cores")
 
 # takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
 dataset = load_dataset("openwebtext")
@@ -23,30 +15,6 @@ dataset = load_dataset("openwebtext")
 # owt by default only contains the 'train' split, so create a test split
 split_dataset = dataset["train"].train_test_split(test_size=0.0005, seed=2357, shuffle=True)
 split_dataset['val'] = split_dataset.pop('test') # rename the test split to val
-
-shard_dataset = DatasetDict()
-for split, dset in split_dataset.items():
-    for i in range(num_shards[split]):
-        shard_dataset[f"{split}_{i:02}"] = dset.shard(num_shards[split], i)
-
-# this results in:
-# >>> shard_dataset
-# DatasetDict({
-#     train_0: Dataset({
-#         features: ['text'],
-#         num_rows: 8009762 / num_shards
-#     })
-#     train_1: Dataset({
-#         features: ['text'],
-#         num_rows: 8009762 / num_shards
-#     })
-#     ...
-#     val_0: Dataset({
-#         features: ['text'],
-#         num_rows: 4007 / num_shards
-#     })
-#     ...
-# })
 
 # we now want to tokenize the dataset. first define the encoding function (gpt2 bpe)
 enc = tiktoken.get_encoding("gpt2")
@@ -57,37 +25,47 @@ def process(example):
     out = {'ids': ids}
     return out
 
-# tokenize the dataset
-tokenized = shard_dataset.map(
+# First process validation set
+print("Processing validation set...")
+val_tokenized = split_dataset['val'].map(
     process,
     remove_columns=['text'],
-    desc="tokenizing the splits",
+    desc="tokenizing validation split",
     num_proc=num_proc,
 )
 
-def _bytes_feature(value):
-  """Returns a bytes_list from a string / byte."""
-  if isinstance(value, type(tf.constant(0))):
-    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+# Save validation set
+print("writing val.bin...")
+val_tokens = np.concatenate([example['ids'] for example in tqdm(val_tokenized)])
+val_tokens = val_tokens.astype(np.uint16)
+val_tokens.tofile('val.bin')
 
-# concatenate all the ids in each dataset into one large file we can use for training
-for split, dset in tokenized.items():
-    filename = f'{split}.tfrecord'
+# Then process training set in chunks
+print("Processing training set...")
+train_tokenized = split_dataset['train'].map(
+    process,
+    remove_columns=['text'],
+    desc="tokenizing training split", 
+    num_proc=num_proc,
+)
 
-    print(f"writing {filename}...")
-    with tf.io.TFRecordWriter(filename) as writer:
-        for example in tqdm(dset):
-            feature = np.asarray(example['ids'], dtype=np.uint16).tobytes()
-            example_proto = tf.train.Example(
-                features=tf.train.Features(feature={
-                    'ids': _bytes_feature(feature)
-                    }))
-            writer.write(example_proto.SerializeToString())
+# Save training set in chunks
+print("writing train.bin...")
+chunk_size = 729_754  # should work on 16GB
+total_chunks = (len(train_tokenized)-1)//chunk_size + 1
 
-    # train.bin is ~17GB, val.bin ~8.5MB
-    # train has ~9B tokens (9,035,582,198)
-    # val has ~4M tokens (4,434,897)
+with open('train.bin', 'wb') as f:
+    for i in tqdm(range(0, len(train_tokenized), chunk_size), 
+                 desc="Processing chunks", 
+                 total=total_chunks):
+        chunk = train_tokenized.select(range(i, min(i + chunk_size, len(train_tokenized))))
+        chunk_tokens = np.concatenate([example['ids'] for example in chunk])
+        chunk_tokens = chunk_tokens.astype(np.uint16)
+        chunk_tokens.tofile(f)
 
-    # to read the bin files later, e.g. with numpy:
-    # m = np.memmap('train.bin', dtype=np.uint16, mode='r')
+# train.bin is ~17GB, val.bin ~8.5MB
+# train has ~9B tokens (9,035,582,198)
+# val has ~4M tokens (4,434,897)
+
+# to read the bin files later, e.g. with numpy:
+# m = np.memmap('train.bin', dtype=np.uint16, mode='r')
